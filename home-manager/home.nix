baebcbb99a7ac08a6ea0c06740ea696195dac565
Olly Seber <olly_s@hotmail.co.uk>
2024-08-02 15:31:03 -0700

    Add librewolf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -63,6 +63,10 @@
     enableCompletion = true;
   };
 
+  programs.librewolf = {
+    enable = true;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
