4a9c80206556cf8841085b6b4b0ade40f5d43c9a
Olly Seber <olly_s@hotmail.co.uk>
2024-08-02 12:27:52 -0700

    Update keymap

--- nixos/hosts/stingray/configuration.nix
+++ nixos/hosts/stingray/configuration.nix
@@ -51,9 +51,9 @@
   };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "euro";
+    variant = "euro";
   };
 
   # Enable CUPS to print documents.
