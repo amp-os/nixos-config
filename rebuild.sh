d59d9a55497855caefc007756f09c5da5dd9e93f
Olly Seber <olly_s@hotmail.co.uk>
2024-08-02 14:41:23 -0700

    Improve rebuild.sh so it can be run from anywhere

--- rebuild.sh
+++ rebuild.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Staging ${SCRIPT_DIR}/settings.nix"
-git add -f ${SCRIPT_DIR}/settings.nix
+(cd ${SCRIPT_DIR}; git add -f settings.nix)
 
 echo "Rebuilding Nixos config"
 nixos-rebuild switch --flake ${SCRIPT_DIR}
 
 echo "Unstaging settings.nix"
-git restore --staged ${SCRIPT_DIR}/settings.nix
+(cd ${SCRIPT_DIR}; git restore --staged settings.nix)
